583cabc081241ebeaced66ea574fc56e63696e1f
Warren-kao <1304854456@qq.com>
2026-07-02 16:42:31 +0800

    矩阵键盘

--- 2.C51/5-1 矩阵键盘/main.c
+++ 2.C51/5-1 矩阵键盘/main.c
@@ -1,20 +1,27 @@
 #include <REGX52.H>
 #include"LCD1602.h"
 #include"Delay.h"
+#include "MatrixKey.h"
+
+unsigned char KeyNum = 16;
 
 void main()
 {
 	int num = 0;
 	LCD_Init();
-	LCD_ShowNum(1,1,666,3);
-	LCD_ShowChar(1,5,'A');
-	LCD_ShowString(2,1,"HELLO WORLD !");
+	LCD_ShowString(1,1,"Key Number:");
 
 	
 	while(1)
 	{
+		KeyNum = MatrixNum();
+
 		
-	
+		if(KeyNum != 16)
+		{
+			LCD_ShowNum(2,1,KeyNum,2);
+		}
+
 	}
 }
 	
