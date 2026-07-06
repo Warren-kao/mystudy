09bff29a4b1e81afd2f1ddcbfc54635a9b35f0a9
Warren-kao <1304854456@qq.com>
2026-07-06 12:43:14 +0800

    update DS1302

--- 2.C51/9-1 DS1302时钟/DS1302.c
+++ 2.C51/9-1 DS1302时钟/DS1302.c
@@ -4,6 +4,11 @@ sbit DS1302_SCK = P3^6;
 sbit DS1302_IO = P3^4;
 sbit DS1302_CE = P3^5;
 
+/**
+  * @brief  DS1302初始化
+  * @param  无
+  * @retval 无
+  */
 void DS1302_Init()
 {
 	DS1302_SCK = 0;
@@ -12,17 +17,32 @@ void DS1302_Init()
 
 }
 
-
+/**
+  * @brief  将BCD格式数转换为十进制整数
+  * @param  byte 需要修改的BCD数
+  * @retval 修改后的十进制整数
+  */
 unsigned char BCDto10(unsigned char byte)
 {
 	return ((byte/16*10)+(byte%16));
 }
 
+/**
+  * @brief  将BCD格式数转换为十进制整数
+  * @param  byte 需要修改十进制整数
+  * @retval 修改后的的BCD数
+  */
 unsigned char _10toBCD(unsigned char byte)
 {
 	return ((byte/10*16)+(byte%10));
 }
 
+/**
+  * @brief  DS1302写入数据
+  * @param  address 需要写入数据的寄存器地址
+  * @param  Data 需要写入的数据
+  * @retval 无
+  */
 void DS1302_WriteData(unsigned char address,Data)
 {
 	unsigned char i;
@@ -45,6 +65,11 @@ void DS1302_WriteData(unsigned char address,Data)
 	DS1302_IO = 0;
 }
 
+/**
+  * @brief  DS1302读取数据
+  * @param  address 需要读取数据的寄存器地址
+  * @retval unsigned char 返回十进制整数
+  */
 unsigned char DS1302_ReadData(unsigned char address)
 {
 	unsigned char Data;
@@ -69,19 +94,34 @@ unsigned char DS1302_ReadData(unsigned char address)
 	return Data = BCDto10(Data);
 }
 
+/**
+  * @brief  将数组内的日期写入DS1302
+  * @param  *arr 需要写入的数组的指针
+  * @retval 无
+  */
 void DS1302_WriteTime(unsigned char *arr)
 {
-	unsigned char i ;
-	for(i = 0;i<7;i++)
-	{
-		DS1302_WriteData(0x8C-(2*i),*(arr+i));
-	}
+	DS1302_WriteData(0x8C,*(arr+0));//年
+	DS1302_WriteData(0x88,*(arr+1));//月
+	DS1302_WriteData(0x86,*(arr+2));//日
+	DS1302_WriteData(0x84,*(arr+3));//时
+	DS1302_WriteData(0x82,*(arr+4));//分
+	DS1302_WriteData(0x80,*(arr+5));//秒
+	DS1302_WriteData(0x8A,*(arr+6));//周
 }
+
+/**
+  * @brief  将DS1302内的日期写入数组
+  * @param  *arr 需要写入的数组的指针
+  * @retval 无
+  */
 void DS1302_readTime(unsigned char *arr)
 {
-	unsigned char i ;
-	for(i = 0;i<7;i++)
-	{
-		*(arr+i) = DS1302_ReadData(0x8D-2*i);
-	}
+	*(arr+0) = DS1302_ReadData(0x8D);//年
+	*(arr+1) = DS1302_ReadData(0x89);//月
+	*(arr+2) = DS1302_ReadData(0x87);//日
+	*(arr+3) = DS1302_ReadData(0x85);//时
+	*(arr+4) = DS1302_ReadData(0x83);//分
+	*(arr+5) = DS1302_ReadData(0x81);//秒
+	*(arr+6) = DS1302_ReadData(0x8B);//周
 }
--- 2.C51/9-1 DS1302时钟/main.c
+++ 2.C51/9-1 DS1302时钟/main.c
@@ -2,9 +2,11 @@
 #include "DS1302.h"
 #include "Delay.h"
 #include "LCD1602.h"
+#include "Timer0.h"
 
 
-unsigned char DS1302_Time []={26,7,7,5,16,30,00};
+
+unsigned char DS1302_Time []={26,7,5,16,30,00,7};
 
 void main()
 {
@@ -16,7 +18,6 @@ void main()
 	LCD_ShowString(2,1,"  :  :");
 	while(1)
 	{
-		DS1302_readTime(DS1302_Time);
 		LCD_ShowNum(1,1,DS1302_Time[0],2);
 		LCD_ShowNum(1,4,DS1302_Time[2],2);
 		LCD_ShowNum(1,7,DS1302_Time[3],2);
@@ -26,4 +27,20 @@ void main()
 		Delay(50);
 
 	}
+}
+
+void Timer0_Rountine() interrupt 1
+{	
+	static unsigned int T0Count;
+	TH0 = 0xFC;
+	TL0 = 0x18;
+	T0Count++;
+	if(T0Count >= 50)
+	{
+		DS1302_readTime(DS1302_Time);
+		
+		T0Count = 0;
+		P2 = ~P2; 
+	}
+	
 }
