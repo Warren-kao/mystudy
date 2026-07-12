0c8e20d3bc514b1448a2396241cb7f5e6946a706
Warren-kao <1304854456@qq.com>
2026-07-12 22:47:39 +0800

    旋转编码器计次

---
+++ 3.STM32/3-2 旋转编码器计次/User/main.c
@@ -0,0 +1,17 @@
+#include "stm32f10x.h"                  // Device header
+#include "OLED.h"
+#include "Encoder.h"
+
+int16_t Num;
+
+int main()
+{
+	OLED_Init();
+	Encoder_Init();
+	OLED_ShowString(1,1,"Warren's");
+	while(1)
+	{
+		Num += Encode_Get();
+		OLED_ShowSignedNum(2,1,Num,5);
+	}
+}
