de5c022ea0cc336fdbafc6b8a353168ace9800fb
Warren-kao <1304854456@qq.com>
2026-08-14 19:24:55 +0800

    位置式PID

---
+++ PID/01- 基础驱动/Hardware/Serial.c
@@ -0,0 +1,120 @@
+#include "stm32f10x.h"                  // Device header
+#include <stdio.h>
+
+uint8_t Serial_RxData,Serial_RxFlag;
+
+void Serial_Init()
+{
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA,ENABLE);
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1,ENABLE);
+	
+	
+	GPIO_InitTypeDef GPIO_InitStructure;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(GPIOA, &GPIO_InitStructure);
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(GPIOA, &GPIO_InitStructure);
+	
+	USART_InitTypeDef USART_InitStruct;
+	USART_InitStruct.USART_BaudRate = 9600;
+	USART_InitStruct.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+	USART_InitStruct.USART_Mode = USART_Mode_Rx|USART_Mode_Tx;
+	USART_InitStruct.USART_Parity = USART_Parity_No;
+	USART_InitStruct.USART_StopBits = USART_StopBits_1;
+	USART_InitStruct.USART_WordLength = USART_WordLength_8b;
+	USART_Init(USART1,&USART_InitStruct);
+	
+	USART_ITConfig(USART1,USART_IT_RXNE,ENABLE);
+	
+	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
+	NVIC_InitTypeDef NVIC_InitStructure;
+	NVIC_InitStructure.NVIC_IRQChannel = USART1_IRQn;
+	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 1;
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 1;
+	NVIC_Init(&NVIC_InitStructure);
+	
+	USART_Cmd(USART1,ENABLE);
+}
+
+void Serial_SendByte(uint8_t Byte)
+{
+	USART_SendData(USART1, Byte);
+	while(USART_GetFlagStatus(USART1,USART_FLAG_TXE) == RESET);
+}
+
+void Serial_SendArr(uint8_t* Arr,uint16_t Size)
+{
+	uint16_t i;
+	for(i = 0;i < Size ; i++)
+	{
+		Serial_SendByte(Arr[i]);
+	}
+}
+
+void Serial_SendString(char* String)
+{
+	while(*String)
+	{
+		Serial_SendByte( *String);
+		while(USART_GetFlagStatus(USART1,USART_FLAG_TXE) == RESET);
+		String++;
+	}
+}
+
+
+uint32_t Serial_Pow(uint32_t x,uint8_t y)
+{
+	uint32_t Ret = 1;
+	while(y--)
+	{
+		Ret *= x;
+	}
+	return Ret;
+}
+
+void Serial_SendNum(uint32_t Num,uint8_t length)
+{
+	uint8_t i;
+	for(i = 0;i < length ; i++)
+	{
+		Serial_SendByte(Num / Serial_Pow(10,length-i-1) % 10 + '0');
+	}
+}
+
+int fputc(int ch,FILE *f)
+{
+	Serial_SendByte(ch);
+	return ch;
+}
+
+uint8_t Serial_GetData()
+{
+	return Serial_RxData;
+}
+
+uint8_t Serial_GetRxFlag()
+{
+	if(Serial_RxFlag == 1)
+	{
+		Serial_RxFlag = 0;
+		return 1;
+	}
+	return 0;
+}
+
+void  USART1_IRQHandler()
+{
+	if(USART_GetFlagStatus(USART1,USART_FLAG_RXNE) == SET)
+	{
+		Serial_RxData = USART_ReceiveData(USART1);
+		Serial_RxFlag = 1;
+		USART_ClearFlag(USART1,USART_FLAG_RXNE);
+	}	
+}
+
+
---
+++ PID/01- 基础驱动/User/main.c
@@ -0,0 +1,242 @@
+#include "stm32f10x.h"                  // Device header
+#include "OLED.h"
+#include "Delay.h"
+#include "LED.h"
+#include "Key.h"
+#include "Timer.h"
+#include "RP.h"
+#include "Motor.h"
+#include "Encoder.h"
+#include "Serial.h"
+
+
+
+//串口测试
+uint16_t RP1,RP2,RP3,RP4;
+int main ()
+{
+	OLED_Init();
+	RP_Init();
+	Serial_Init();
+	while(1)
+	{
+		RP1 = RP_GetValue(1);
+		RP2 = RP_GetValue(2);
+		RP3 = RP_GetValue(3);
+		RP4 = RP_GetValue(4);
+		OLED_Printf(0,0,OLED_8X16,"RP1:%04d",RP1);
+		OLED_Printf(0,16,OLED_8X16,"RP2:%04d",RP2);
+		OLED_Printf(0,32,OLED_8X16,"RP3:%04d",RP3);
+		OLED_Printf(0,48,OLED_8X16,"RP4:%04d",RP4);
+		OLED_Update();
+		
+		printf("%d,%d,%d,%d\n",RP1,RP2,RP3,RP4);
+		Delay_ms(10);
+	}
+}
+
+
+
+
+//编码器测试
+//int16_t Speed;
+//int16_t Location;
+//int main ()
+//{
+//	OLED_Init();
+//	Encoder_Init();
+//	Timer_Init();
+//	while(1)
+//	{
+//		
+
+//		OLED_Printf(0,0,OLED_8X16,"Speed:%+05d",Speed);
+//		OLED_Printf(0,16,OLED_8X16,"Location:%+05d",Location);
+//		
+//		
+//		OLED_Update();
+//	}
+//}
+
+//void TIM1_UP_IRQHandler()
+//{
+//	static uint16_t count;
+//	if(TIM_GetFlagStatus(TIM1,TIM_IT_Update) == SET)
+//	{
+//		count++;
+//		if(count >= 40)
+//		{
+//			Speed = Encoder_Get();
+//			Location += Speed;
+//		}
+//		TIM_ClearFlag(TIM1,TIM_IT_Update);
+//	}
+//	
+//}
+
+
+//电机测试
+//uint8_t KeyNum;
+//int16_t PWM;
+//int main ()
+//{
+//	OLED_Init();
+
+//	Key_Init();
+//	Motor_Init();
+//	Timer_Init();
+//	while(1)
+//	{
+//		if(Key_Check(KEY_1,KEY_UP))
+//		{
+//			PWM += 10;
+//			if(PWM > 100)
+//				PWM = 100;
+//		}else if(Key_Check(KEY_2,KEY_UP))
+//		{
+//			PWM -= 10;
+//			if(PWM < -100)
+//				PWM = -100;
+//		}else if(Key_Check(KEY_3,KEY_UP))
+//		{
+//			PWM = 0;
+//		}else if(Key_Check(KEY_4,KEY_UP))
+//		{
+//			PWM = -PWM;
+//		}
+
+//		OLED_Printf(0,0,OLED_8X16,"PWM:%04d",PWM);
+//		Motor_SetPWM(PWM);
+//		
+//		OLED_Update();
+//	}
+//}
+
+//void TIM1_UP_IRQHandler()
+//{
+//	if(TIM_GetFlagStatus(TIM1,TIM_IT_Update) == SET)
+//	{
+//		Key_Tick();
+//		
+//		TIM_ClearFlag(TIM1,TIM_IT_Update);
+//	}
+//	
+//}
+
+
+//电位器旋钮测试
+//uint16_t i,j;
+//int main ()
+//{
+//	OLED_Init();
+//	RP_Init();
+//	while(1)
+//	{
+//		OLED_Printf(0,0,OLED_8X16,"RP1:%04d",RP_GetValue(1));
+//		OLED_Printf(0,16,OLED_8X16,"RP2:%04d",RP_GetValue(2));
+//		OLED_Printf(0,32,OLED_8X16,"RP3:%04d",RP_GetValue(3));
+//		OLED_Printf(0,48,OLED_8X16,"RP4:%04d",RP_GetValue(4));
+//		OLED_Update();
+//	}
+//}
+
+
+//按键测试
+//uint16_t i,j;
+//int main ()
+//{
+//	OLED_Init();
+//	Timer_Init();
+//	Key_Init();
+//	while(1)
+//	{
+//		if(Key_Check(KEY_1,KEY_UP))
+//		{
+//			j ++;
+//		}else if(Key_Check(KEY_2,KEY_UP))
+//		{
+//			j --;
+//		}else if(Key_Check(KEY_3,KEY_UP))
+//		{
+//			j = 0;
+//		}else if(Key_Check(KEY_4,KEY_UP))
+//		{
+//			j =999;
+//		}
+//		OLED_ShowNum(0,0,i,5,OLED_8X16);
+//		OLED_ShowNum(0,16,j,5,OLED_8X16);
+//		OLED_Update();
+//	}
+//}
+
+//void TIM1_UP_IRQHandler()
+//{
+//	if(TIM_GetFlagStatus(TIM1,TIM_IT_Update) == SET)
+//	{
+//		Key_Tick();
+//		i++;
+//		TIM_ClearFlag(TIM1,TIM_IT_Update);
+//	}
+//	
+//}
+
+
+
+//定时中断测试
+
+//uint16_t i;
+//int main ()
+//{
+//	OLED_Init();
+//	Timer_Init();
+//	while(1)
+//	{
+//		OLED_ShowNum(0,0,i,5,OLED_8X16);
+//		OLED_Update();
+//	}
+//}
+
+//void TIM1_UP_IRQHandler()
+//{
+//	if(TIM_GetFlagStatus(TIM1,TIM_IT_Update) == SET)
+//	{
+//		
+//		i++;
+//		TIM_ClearFlag(TIM1,TIM_IT_Update);
+//	}
+//	
+//}
+
+
+
+//LED测试
+//int main ()
+//{
+//	LED_Init();
+//	
+//	while(1)
+//	{
+//		LED_ON();
+//		Delay_ms(500);
+//		LED_OFF();
+//		Delay_ms(500);
+//		LED_Turn();
+//		Delay_ms(500);
+//		LED_Turn();
+//		Delay_ms(500);
+//	}
+//}
+
+
+
+//OLED测试
+//int main()
+//{
+//	OLED_Init();
+//	OLED_ShowString(0,0,"hello",OLED_8X16);
+//	OLED_Update();
+//	while(1)
+//	{
+
+//	}
+//}
