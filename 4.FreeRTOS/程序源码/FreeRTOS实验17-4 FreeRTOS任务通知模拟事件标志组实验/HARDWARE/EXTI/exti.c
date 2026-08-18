43e3c2f266a27cd822e8de96a4a8b97a1cc89725
Warren-kao <1304854456@qq.com>
2026-08-18 21:22:13 +0800

    freertos任务创建与删除

---
+++ 4.FreeRTOS/程序源码/FreeRTOS实验17-4 FreeRTOS任务通知模拟事件标志组实验/HARDWARE/EXTI/exti.c
@@ -0,0 +1,69 @@
+#include "exti.h"
+#include "led.h"
+#include "key.h"
+#include "delay.h"
+#include "usart.h"
+#include "FreeRTOS.h"
+#include "event_groups.h"
+//////////////////////////////////////////////////////////////////////////////////	 
+//本程序只供学习使用，未经作者许可，不得用于其它任何用途
+//Mini STM32开发板
+//外部中断 驱动代码			   
+//正点原子@ALIENTEK
+//技术论坛:www.openedv.com
+//修改日期:2010/12/01  
+//版本：V1.0
+//版权所有，盗版必究。
+//Copyright(C) 正点原子 2009-2019
+//All rights reserved	  
+////////////////////////////////////////////////////////////////////////////////// 	  
+ 
+#define EVENTBIT_0 (1<<0)
+
+//外部中断初始化函数
+void EXTIX_Init(void)
+{
+ 
+	EXTI_InitTypeDef EXTI_InitStructure;	
+	NVIC_InitTypeDef NVIC_InitStructure;
+
+  	RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO,ENABLE);//外部中断，需要使能AFIO时钟
+
+	KEY_Init();//初始化按键对应io模式
+
+    //GPIOC.5 中断线以及中断初始化配置
+  	GPIO_EXTILineConfig(GPIO_PortSourceGPIOC,GPIO_PinSource5);
+
+  	EXTI_InitStructure.EXTI_Line=EXTI_Line5;
+  	EXTI_InitStructure.EXTI_Mode = EXTI_Mode_Interrupt;	
+  	EXTI_InitStructure.EXTI_Trigger = EXTI_Trigger_Falling;//下降沿触发
+  	EXTI_InitStructure.EXTI_LineCmd = ENABLE;
+  	EXTI_Init(&EXTI_InitStructure);	 	//根据EXTI_InitStruct中指定的参数初始化外设EXTI寄存器
+ 
+		
+	NVIC_InitStructure.NVIC_IRQChannel = EXTI9_5_IRQn;			//使能按键所在的外部中断通道
+  	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0x06;//抢占优先级6 
+  	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0x00;		//子优先级0
+  	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;				//使能外部中断通道
+  	NVIC_Init(&NVIC_InitStructure); 
+}
+
+
+//任务句柄
+extern TaskHandle_t EventGroupTask_Handler;
+BaseType_t xHigherPriorityTaskWoken;
+
+void EXTI9_5_IRQHandler(void)
+{	
+	delay_xms(20);   //消抖			 
+	if(KEY0==0)	
+	{
+		xTaskNotifyFromISR((TaskHandle_t	)EventGroupTask_Handler, 	//任务句柄
+						   (uint32_t		)EVENTBIT_0, 				//要更新的bit
+						   (eNotifyAction	)eSetBits, 					//更新指定的bit
+						   (BaseType_t*		)xHigherPriorityTaskWoken);
+
+		portYIELD_FROM_ISR(xHigherPriorityTaskWoken);
+	}
+ 	 EXTI_ClearITPendingBit(EXTI_Line5);    //清除LINE5上的中断标志位  
+}
